e69bc50c4b3988c42c9e16e4534b0fe76d82fcfd
Eric Diplpe <erdipple@microsoft.com>
2022-03-25 10:58:59 -0400

    Latest code.

--- cds/Project-Operations/ProjectIntegration/Program.cs
+++ cds/Project-Operations/ProjectIntegration/Program.cs
@@ -14,9 +14,11 @@ namespace PowerApps.Samples {
     //Controls the max degree of parallelism
     static readonly int maxDegreeOfParallelism = 10;
     //Total number of projects to create
-    static readonly int numberOfProjectsToCreate = 100;
+    static readonly int numberOfProjectsToCreate = 1000;
     //Number of tasks per project.
     static readonly int numberOfTasksPerProject = 10;
+    //Number of assignments per task
+    static readonly int numberOfAssignmentsPerTask = 3;
 
     static void Main(string[] args) {
       #region Optimize Connection
@@ -37,6 +39,15 @@ namespace PowerApps.Samples {
 
       var count = 0;
       double secondsToComplete;
+
+      // This is not using the preferred AsyncParallelOperations model.  The goals is to allow you to
+      // see how the individual calls to the ScheduleAPIs are processed so we will use a simple threading
+      // model to run 10 threads in parallel.  Only using 1 systemuser account for authentication so at max
+      // we can have 10 operationsets processing at a time.  Thus 10 threads where each thread will execute
+      // the operationsets synchronously.  While each thread is running synchronously have 10 threads allows
+      // the execution of 10 operationsets in parallel.
+
+
     }
   }
 }
